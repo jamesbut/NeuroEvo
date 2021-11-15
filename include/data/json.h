4ea7806a495c515e778311a6c3f6294348a3642f
James Butterworth <jjbsports16@hotmail.co.uk>
2021-11-15 13:11:35 +0000

    New json files

---
+++ include/data/json.h
@@ -0,0 +1,20 @@
+#ifndef _JSON_H_
+#define _JSON_H_
+
+/*
+ * An interfact for interacting with json files
+ */
+
+#include <string>
+
+class JSON
+{
+
+public:
+
+    JSON(const std::string& file_path);
+
+
+};
+
+#endif
---
+++ src/data/json.cpp
@@ -0,0 +1,8 @@
+#include <data/json.h>
+
+#include <iostream>
+
+JSON::JSON(const std::string& file_name)
+{
+    std::cout << file_name << std::endl;
+}
