b15764743da40b594cc93dbef6c5075ec43268d1
James Butterworth <jjbsports16@hotmail.co.uk>
2021-11-15 16:46:08 +0000

    Read only access to jso

--- include/data/json.h
+++ include/data/json.h
@@ -7,6 +7,9 @@
 
 #include <string>
 #include <nlohmann/json.hpp>
+#include <iostream>
+
+namespace NeuroEvo {
 
 class JSON
 {
@@ -15,6 +18,11 @@ public:
 
     JSON(const std::string& file_path);
 
+    //Give read only access to nlohmann::json object
+    const nlohmann::json& get() const;
+
+    friend std::ostream& operator<<(std::ostream& s, const JSON& json);
+
 private:
 
     nlohmann::json read_json(const std::string& file_path);
@@ -22,5 +30,7 @@ private:
     nlohmann::json _j;
 
 };
+
+} // namespace NeuroEvo
 
 #endif
--- src/data/json.cpp
+++ src/data/json.cpp
@@ -2,18 +2,26 @@
 #include <fstream>
 #include <iostream>
 
+namespace NeuroEvo {
 
-JSON::JSON(const std::string& file_name) : _j(read_json(file_name))
-{
-    std::cout << std::setw(4) << _j << std::endl;
-}
+JSON::JSON(const std::string& file_name) : _j(read_json(file_name)) {}
 
 nlohmann::json JSON::read_json(const std::string& file_path)
 {
     std::ifstream json_file(file_path);
     if(json_file.fail())
-    {
         throw std::ios_base::failure("Cannot read json at " + file_path);
-    }
     return nlohmann::json::parse(json_file);
 }
+
+const nlohmann::json& JSON::get() const
+{
+    return _j;
+}
+
+std::ostream& operator<<(std::ostream& s, const JSON& json)
+{
+    return s << std::setw(4) << json._j << std::endl;
+}
+
+} // namespace NeuroEvo
