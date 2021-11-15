49338dde6fe86f10d633c6561729146bd068c3fa
James Butterworth <jjbsports16@hotmail.co.uk>
2021-11-15 14:39:45 +0000

    Included nlohmann json

--- src/data/json.cpp
+++ src/data/json.cpp
@@ -1,8 +1,12 @@
 #include <data/json.h>
+#include <nlohmann/json.hpp>
+#include <fstream>
 
 #include <iostream>
 
 JSON::JSON(const std::string& file_name)
 {
     std::cout << file_name << std::endl;
+    std::ifstream json_file(file_name);
+    nlohmann::json j;
 }
